a840df2fb6e7685594d1b32d95e682a8898fd593
poly1997p <poly20221997@gmail.com>
2025-04-19 12:39:38 +0600

    insert data

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ include 'config.php';
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="create.php">Insert Data</a>
           </li>
 
         </ul>
